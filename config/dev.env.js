73d6fa3603b6fa87667e3bb5a59ea8027901fd28
fermaud <robin.fermaud@epitech.eu>
2018-10-21 21:58:46 +0200

    credentials settings

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,7 +8,7 @@ module.exports = merge(prodEnv, {
     // Amazon S3 Upload File
     AMAZON_S3_ACCESS_KEY_ID: '""',
     AMAZON_S3_SECRET_ACCESSS_KEY:  '""',
-    AMAZON_S3_BUCKET: '""',
-    AMAZON_S3_REGION: '"eu-west-3"',
+    AMAZON_S3_BUCKET: '""', // Your Bucket Name
+    AMAZON_S3_REGION: '"eu-west-3"', // Change it if you need
     AMAZON_S3_URL: '"https://s3.eu-west-3.amazonaws.com/"'
 })
